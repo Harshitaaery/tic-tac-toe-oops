575ad65538ccfc93e7833fc6d7a24980489d2d41
Harshitaaery <aeryharshita61@gmail.com>
2025-09-12 21:46:44 +0530

    setup

---
+++ src/com/tictactoe/tictactoe.java
@@ -0,0 +1,161 @@
+import java.util.Scanner;
+
+// Player Class
+class Player {
+    private String name;
+    private char symbol; // 'X' or 'O'
+
+    public Player(String name, char symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
+
+// Board Class
+class Board {
+    private char[][] grid;
+    private final int SIZE = 3;
+
+    public Board() {
+        grid = new char[SIZE][SIZE];
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                grid[i][j] = ' ';
+            }
+        }
+    }
+
+    public void display() {
+        System.out.println("-------------");
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(grid[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    public boolean makeMove(int row, int col, char symbol) {
+        if (row >= 0 && row < SIZE && col >= 0 && col < SIZE && grid[row][col] == ' ') {
+            grid[row][col] = symbol;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isWinner(char symbol) {
+        // Check rows
+        for (int i = 0; i < SIZE; i++) {
+            if (grid[i][0] == symbol && grid[i][1] == symbol && grid[i][2] == symbol) {
+                return true;
+            }
+        }
+        // Check columns
+        for (int j = 0; j < SIZE; j++) {
+            if (grid[0][j] == symbol && grid[1][j] == symbol && grid[2][j] == symbol) {
+                return true;
+            }
+        }
+        // Check diagonals
+        if (grid[0][0] == symbol && grid[1][1] == symbol && grid[2][2] == symbol) {
+            return true;
+        }
+        if (grid[0][2] == symbol && grid[1][1] == symbol && grid[2][0] == symbol) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (grid[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+// Game Class
+class Game {
+    private Board board;
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private Scanner scanner;
+
+    public Game(Player p1, Player p2) {
+        this.board = new Board();
+        this.player1 = p1;
+        this.player2 = p2;
+        this.currentPlayer = p1;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void play() {
+        System.out.println("Welcome to Tic-Tac-Toe!");
+        while (true) {
+            board.display();
+            System.out.println(currentPlayer.getName() + "'s turn (" + currentPlayer.getSymbol() + ")");
+            System.out.print("Enter row (0-2) and column (0-2): ");
+            
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+
+            if (board.makeMove(row, col, currentPlayer.getSymbol())) {
+                if (board.isWinner(currentPlayer.getSymbol())) {
+                    board.display();
+                    System.out.println(currentPlayer.getName() + " wins!");
+                    break;
+                }
+                if (board.isFull()) {
+                    board.display();
+                    System.out.println("It's a draw!");
+                    break;
+                }
+                switchTurn();
+            } else {
+                System.out.println("Invalid move, try again.");
+            }
+        }
+    }
+
+    private void switchTurn() {
+        currentPlayer = (currentPlayer == player1) ? player2 : player1;
+    }
+}
+
+// Main Class
+public class TicTacToe {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name for Player 1: ");
+        String name1 = sc.nextLine();
+        Player p1 = new Player(name1, 'X');
+
+        System.out.print("Enter name for Player 2: ");
+        String name2 = sc.nextLine();
+        Player p2 = new Player(name2, 'O');
+
+        Game game = new Game(p1, p2);
+        game.play();
+
+        sc.close();
+    }
+}
